af58fcae88c072dea75a41f4c1a27ba2fda355f0
sravanin15 <sravaninuthanapati15@gmail.com>
2021-02-05 10:46:00 +0530

    commit  second branch

--- challenge1/src/challenge1/HelloWorld.java
+++ challenge1/src/challenge1/HelloWorld.java
@@ -3,6 +3,6 @@ package challenge1;
 public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("Hello world");
-	System.out.println("Hello mars");
+	System.out.println("Hello mars ");
 }
 }
